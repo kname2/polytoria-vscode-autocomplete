eee4c4de6c7f230d9859caab16a4d703fcda2a47
kname2 <nbinhc@gmail.com>
2026-02-07 22:35:06 -0500

    vars almost done

--- src/extension.ts
+++ src/extension.ts
@@ -1,51 +1,71 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { getValidVars, getVarType, getVarValue } from './variableFuncs'
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	const activators: string[] = []
+	const activators: string[] = [];
 	let provider = vscode.languages.registerCompletionItemProvider('lua', {
 		provideCompletionItems(document, position, token, context) {
 			let completionItems = undefined;
-			const prefix = document.lineAt(position).text.substring(0,position.character);
+			let prefix = document.lineAt(position).text.substring(0,position.character);
 
 			// VARIABLE RECOGNITION
+			const validVars: vscode.DocumentSymbol[] = getValidVars(document, position);
+
+			let matchedVar = undefined
+			let varType = undefined
+			for (const variable of validVars) {
+				if (prefix.endsWith(variable.name+'.') || prefix.endsWith(variable.name+':') || prefix.endsWith(variable.name+'[')) {
+					if (prefix.indexOf('local '+variable.name) == -1) {
+						matchedVar = variable
+						const varValue: string = getVarValue(document,variable)
+						let endPos = prefix.indexOf(' '+variable.name)
+						if (endPos === -1) {
+							endPos = prefix.indexOf('\t'+variable.name)
+						}
+						prefix = prefix.substring(0, endPos)+varValue
+						varType = getVarType(document, variable)
+					}
+				}
+			}
+			// variables refered to inside variables are not translated.    todo: program large limited depth
 			
 
 			// MAIN CLASSES, STATIC CLASSES, AND CONSTRUCTORS
 			const polyStaticsAndClasses: string[] = [
 				"Achievements", "Chat", "CoreUI", "Datastore", "game",
 				"Http", "Input", "Insert", "Json", "Tween"
-			]
+			];
 			const polyConstructors: string[] = [
 				"Color", "ColorRange", "NetMessage", "NumberRange",
 				"Vector2", "Vector3"
-			]
+			];
 
-			if (prefix.trim().length == 1) {
-				let alliterativeList: string[] = []
+			if (prefix.trim().length === 1) {
+				let alliterativeList: string[] = [];
 				for (const pClass of polyStaticsAndClasses) {
-					if (prefix.trim().toLowerCase == pClass.substring(0,0).toLowerCase) {
-						alliterativeList.push(pClass)
+					if (prefix.trim().toLowerCase === pClass.substring(0,0).toLowerCase) {
+						alliterativeList.push(pClass);
 					}
 				}
-				if (alliterativeList.length != 0) {
+				if (alliterativeList.length !== 0) {
 					completionItems = [];
 
 					for (const pClass of alliterativeList) {
 						const startPosition = new vscode.Position(position.line, position.character - 1);
 						const repRange = new vscode.Range(startPosition, position);
 
-						let item = new vscode.CompletionItem(pClass, vscode.CompletionItemKind.Class)
-						item.range = repRange
+						let item = new vscode.CompletionItem(pClass, vscode.CompletionItemKind.Class);
+						item.range = repRange;
 
-						completionItems.push(item)
+						completionItems.push(item);
 					}
 
 				}
 				
-				return completionItems
+				return completionItems;
 				
 			}
 
@@ -67,7 +87,7 @@ export function activate(context: vscode.ExtensionContext) {
 				for (const compTxt of completionClasses) {
 					let item = new vscode.CompletionItem(compTxt, vscode.CompletionItemKind.Class);
 					item.insertText = '["'+compTxt+'"]';
-					if (compTxt == "") {
+					if (compTxt === "") {
 						item = new vscode.CompletionItem('[""]', vscode.CompletionItemKind.Class);
 						item.insertText = new vscode.SnippetString('["${1}"]');
 					};
@@ -82,7 +102,7 @@ export function activate(context: vscode.ExtensionContext) {
 					completionItems.push(item);
 				}
 
-				return completionItems
+				return completionItems;
 			};
 			if (prefix.endsWith('["Eviroment"].') || prefix.endsWith('["Eviroment"][')) {
 				
@@ -91,19 +111,19 @@ export function activate(context: vscode.ExtensionContext) {
 			// EVENTS
 			const polyEvents: string[] = [
 				'Loaded', "... todo"
-			]
+			];
 			for (const pEvent of polyEvents) {
 				if (prefix.endsWith('.'+pEvent+'.') || prefix.endsWith('.'+pEvent+':')) {
 					const startPosition = new vscode.Position(position.line, position.character - 1);
 					const repRange = new vscode.Range(startPosition, position);
 
-					let connectItem = new vscode.CompletionItem(':Connect()', vscode.CompletionItemKind.Function)
-					connectItem.insertText = new vscode.SnippetString(':Connect(${1})')
-					connectItem.range = repRange
+					let connectItem = new vscode.CompletionItem(':Connect()', vscode.CompletionItemKind.Function);
+					connectItem.insertText = new vscode.SnippetString(':Connect(${1})');
+					connectItem.range = repRange;
 
-					let disconnectItem = new vscode.CompletionItem(':Disconnect()', vscode.CompletionItemKind.Function)
-					disconnectItem.insertText = new vscode.SnippetString(':Disconnect(${1})')
-					disconnectItem.range = repRange
+					let disconnectItem = new vscode.CompletionItem(':Disconnect()', vscode.CompletionItemKind.Function);
+					disconnectItem.insertText = new vscode.SnippetString(':Disconnect(${1})');
+					disconnectItem.range = repRange;
 				};
 			};
 
--- src/variableFuncs.ts
+++ src/variableFuncs.ts
@@ -7,16 +7,16 @@ export async function getValidVars(
     const symbols = await vscode.commands.executeCommand<vscode.DocumentSymbol[]>(
         'vscode.executeDocumentSymbolProvider',
         document.uri
-    )
+    );
 
-    if (!symbols) return [];
+    if (!symbols) {return [];}
 
-    let validVars: vscode.DocumentSymbol[] = []
+    let validVars: vscode.DocumentSymbol[] = [];
 
     for (const symb of symbols) {
         if (symb.range.contains(pos)) {
             if (symb.kind === vscode.SymbolKind.Variable || symb.kind === vscode.SymbolKind.Constant) {
-                validVars.push(symb)
+                validVars.push(symb);
             }
         }
     }
@@ -27,26 +27,45 @@ export async function getVarValue(
     document: vscode.TextDocument,
     variable: vscode.DocumentSymbol
 ) {
-    
+    const pos = new vscode.Position(variable.range.start.line, document.lineAt(variable.range.start.line).text.indexOf(variable.name));
+    const definitions = await vscode.commands.executeCommand<vscode.Location[]>(
+        'vscode.executeDefinitionProvider',
+        document.uri,
+        pos
+    );
+    if (definitions && definitions.length > 0) {
+        const def = definitions[0];
+        const document = await vscode.workspace.openTextDocument(def.uri);
+        const rawText = document.getText(def.range);
+
+        // single lineifier
+        const normDef = rawText
+            .replace(/[\r\n]+/g, ' ') 
+            .replace(/\s\s+/g, ' ')   
+            .trim();
+
+        return normDef
+    }
+    return undefined
 }
 
 export async function getVarType(
     document: vscode.TextDocument,
     variable: vscode.DocumentSymbol
 ) {    
-    const line = document.lineAt(variable.range.start.line).text.trim()
+    const line = document.lineAt(variable.range.start.line).text.trim();
 
     //separate constructors and typings
-    const suffix = line.substring(line.indexOf(variable.name)+variable.name.length).trim().toLowerCase()
+    const suffix = line.substring(line.indexOf(variable.name)+variable.name.length).trim().toLowerCase();
     if (suffix.indexOf(':') < 2) {
         //type
         const validTypes: string[] = [
         'color', 'colorrange', 'event', 'netmessage', 'numberrange',
         'number', 'rayresult', 'vector2', 'vector3', 'signal'
-        ] 
+        ];
         for (const type of validTypes) {
             if (suffix.indexOf(type) < 5) {
-                return type
+                return type;
             }
         }
     }
@@ -55,11 +74,21 @@ export async function getVarType(
         const validConstructors: string[] = [
             'color', 'colorrange', 'netmessage', 'numberrange',
             'vector2', 'vector3'
-        ]
+        ];
         for (const constructor of validConstructors) {
             if (suffix.indexOf(constructor) < 5) {
-                return constructor
+                return constructor;
+            }
+        }
+
+        const numbers: string[] = [
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+        ]
+        for (const num of numbers) {
+            if (suffix.indexOf(num) < 5) {
+                return 'number';
             }
         }
     }
+    return undefined
 }
